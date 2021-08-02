594e19dcab9ca5bb34fa25a5d20636326c3e385b
Sagar <sagar.j@akademe.in>
2021-08-02 21:32:40 +0530

    Initial commit

---
+++ src/views/Task1/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./task1.scss";
+
+class Task1 extends Component {
+  state = {
+    value: "",
+    boxArray: [],
+  };
+
+  render() {
+    const { value } = this.state;
+    let arr = value.split("");
+    return (
+      <div className="task1-cont">
+        <input
+          type="text"
+          value={value}
+          onChange={(ev) => this.setState({ value: ev.target.value })}
+          className="form-input"
+        />
+
+        <div className="box-cont">
+          {arr.map((d) => (
+            <div className="box">{d}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Task1;
